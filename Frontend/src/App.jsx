ac3b3a060ea7326ec544ceee6107150783703a11
abhigit23 <abhijeetbhandari84@gmail.com>
2024-03-16 11:47:54 +0530

    Add licensing form

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,37 @@
 // import SuperAdminRegistrationForm from "./components/SuperAdminRegistrationForm";
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';//importing BrowserRouter, Routes, Route from react-router-dom
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"; //importing BrowserRouter, Routes, Route from react-router-dom
 
-import BillingForm from './components/BillingForm';
+import BillingForm from "./components/BillingForm";
+import ClientsDetails from "./components/ClientsDetails";
+import Licensing from "./components/Licensing";
+import LoginPage from "./components/LoginPage";
 // import GoldPage from './pages/GoldPage';
 // import PlatinumPage from './pages/PlatinumPage';
 // import FreePage from './pages/FreePage';
-import PricingPage from './components/PricingPage';
-import RegistrationForm from './components/RegistrationForm';
-import LoginPage from './components/LoginPage';
-import ClientsDetails from './components/ClientsDetails';
-import SuperAdminRegistration from './components/SuperAdminRegistration';
-import SuperAdminLogin from './components/SuperAdminLogin';
+import PricingPage from "./components/PricingPage";
+import RegistrationForm from "./components/RegistrationForm";
+import SuperAdminLogin from "./components/SuperAdminLogin";
+import SuperAdminRegistration from "./components/SuperAdminRegistration";
 // import DisplayAllClientDetails from './components/DisplayAllClientDetails';
 
 function App() {
-  return (
-    <>
-    <Router>
-      <Routes>
-      <Route element={<RegistrationForm />} path='signup'/>
-      <Route element={<LoginPage />} path='login'/>
-       <Route element={<PricingPage />} path='pricing'/>
-        <Route element={<BillingForm />} path='billing'/>
-        {/* <Route element={<DisplayAllClientDetails />} path='displayclients'/> */}
-        <Route element={<ClientsDetails />} path='clientsdetails'/>
-        <Route element = {<SuperAdminRegistration/>} path='superadminsignup'/>
-        <Route element = {<SuperAdminLogin/>} path='superadminlogin'/>
-      </Routes>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route element={<RegistrationForm />} path="signup" />
+					<Route element={<LoginPage />} path="login" />
+					<Route element={<PricingPage />} path="pricing" />
+					<Route element={<BillingForm />} path="billing" />
+					{/* <Route element={<DisplayAllClientDetails />} path='displayclients'/> */}
+					<Route element={<ClientsDetails />} path="clientsdetails" />
+					<Route element={<SuperAdminRegistration />} path="superadminsignup" />
+					<Route element={<SuperAdminLogin />} path="superadminlogin" />
+					<Route element={<Licensing />} path="licensing" />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
---
+++ Frontend/src/components/Licensing.jsx
@@ -0,0 +1,97 @@
+import { Space, Table, Tag } from "antd";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+const columns = [
+	{
+		title: "Company Name",
+		dataIndex: "companyname",
+		key: "companyname",
+		render: (text) => <a>{text}</a>,
+	},
+	{
+		title: "Plan",
+		dataIndex: "plan",
+		key: "plan",
+	},
+	{
+		title: "Start Date",
+		dataIndex: "createdAt",
+		key: "createdAt",
+	},
+
+	{
+		title: "Expiry Date",
+		dataIndex: "expirationDate",
+		key: "expirationDate",
+	},
+	{
+		title: "Email",
+		dataIndex: "companyemailid",
+		key: "companyemailid",
+	},
+
+	{
+		title: "Phone",
+		dataIndex: "phonenumber",
+		key: "phonenumber",
+	},
+];
+
+const Licensing = () => {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const fetchDetails = async () => {
+			const licenseDetails = await axios.get(
+				"http://localhost:3000/licencedetails",
+			);
+
+			setData(licenseDetails.data.data);
+		};
+		fetchDetails();
+	}, []);
+
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				backgroundColor: " #EAEDED ",
+				height: "100vh",
+				alignItems: "center",
+			}}
+		>
+			<div
+				style={{
+					width: "60%",
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "center",
+					marginTop: "50px",
+					backgroundColor: "#FDFEFE",
+					borderRadius: "20px",
+				}}
+			>
+				<h2
+					style={{
+						textAlign: "center",
+						fontFamily: "monospace",
+						padding: "10px",
+					}}
+				>
+					License Details
+				</h2>
+				<Table
+					bordered
+					columns={columns}
+					rowKey={() => `${crypto.randomUUID()}`}
+					dataSource={data}
+				/>
+				;
+			</div>
+		</div>
+	);
+};
+
+export default Licensing;
