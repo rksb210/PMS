34dfc69e51327f8a217c91a8b2fc28e9a4594189
abhigit23 <abhijeetbhandari84@gmail.com>
2024-03-16 11:55:27 +0530

    Add env variables

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
-require('./models/index')
+require("./models/index");
 // const registration = require('./models/registrationModel')
 // registration.sync({force:true})
 
@@ -16,9 +17,7 @@ app.use(require("./routes/billing"));
 app.use(require("./routes/superadmin"));
 app.use(require("./routes/licenceDetails"));
 
-
-
 app.listen(3000, () => {
-  //listening on port 5000
-  console.log("Server has started on port 3000");
+	//listening on port 5000
+	console.log("Server has started on port 3000");
 });
--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,16 +1,21 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize("pms", "node", "Rajkishore@210", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
+const sequelize = new Sequelize(
+	"pms",
+	process.env.SQLUSER,
+	process.env.SQLPASSWORD,
+	{
+		host: "localhost",
+		dialect: "mysql",
+		logging: false,
+	},
+);
 
 try {
-  sequelize.authenticate();
-  console.log("Connection has been established successfully.");
+	sequelize.authenticate();
+	console.log("Connection has been established successfully.");
 } catch (error) {
-  console.error("Unable to connect to the database:", error);
+	console.error("Unable to connect to the database:", error);
 }
 const db = {};
 db.Sequelize = Sequelize;
@@ -21,9 +26,9 @@ db.pricing = require("./pricingModel")(sequelize, DataTypes);
 db.billing = require("./billingModel")(sequelize, DataTypes);
 db.superadmin = require("./superAdminRegistrationModel")(sequelize, DataTypes);
 
-db.registration.hasOne(db.billing,{ foreignKey :"registration_id"});
-db.billing.belongsTo(db.registration,{ foreignKey :"registration_id"});
+db.registration.hasOne(db.billing, { foreignKey: "registration_id" });
+db.billing.belongsTo(db.registration, { foreignKey: "registration_id" });
 
-db.sequelize.sync({force:false});
+db.sequelize.sync({ force: false });
 
 module.exports = db;
