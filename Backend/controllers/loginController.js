1293945819c2155f3e1ab9a590f940599255e75c
abhigit23 <abhijeetbhandari84@gmail.com>
2024-03-18 18:36:22 +0530

    Add client dashboard

--- Backend/controllers/loginController.js
+++ Backend/controllers/loginController.js
@@ -1,40 +1,39 @@
-var db = require("../models/index");
-var Registration = db.registration;
+const db = require("../models/index");
+const Registration = db.registration;
 
 const logindata = async (req, res) => {
-  // console.log("reqqqq:", req.body);
-  const { companyemailid, password } = req.body;
-  const login = await Registration.findAll({
-    where: {
-      companyemailid: companyemailid,
-      password: password,
-    },
-  
-  },
-  {raw:true});         /////////////to remove extra information
-  
-  console.log("logggginnn:", login);
-  console.log("logggginLen:", login[0].registration_id);
-  
-    if (login.length===1) {
-      res
-        .status(200)
-        .json({ message: "Success", status: true, data: login[0] });
-    } else {
-      res.json({
-        message: "Invalid Login Credentials",
-        status: false,
-      });
-    }
-  // } else {
-  //   res.status(200).json({ message: "Database Error", status: false });
-  // }
+	// console.log("reqqqq:", req.body);
+	const { companyemailid, password } = req.body;
+	const login = await Registration.findAll(
+		{
+			where: {
+				companyemailid: companyemailid,
+				password: password,
+			},
+		},
+		{ raw: true },
+	); /////////////to remove extra information
 
-  // if (login) {
-  //   res.status(200).json({ message: "Success", status: true, data: login });
-  // } else {
-  //   res.status(200).json({ message: "Invalid Email/Password", status: false });
-  // }
+	console.log("logggginnn:", login);
+	console.log("logggginLen:", login[0].registration_id);
+
+	if (login.length === 1) {
+		res.status(200).json({ message: "Success", status: true, data: login[0] });
+	} else {
+		res.json({
+			message: "Invalid Login Credentials",
+			status: false,
+		});
+	}
+	// } else {
+	//   res.status(200).json({ message: "Database Error", status: false });
+	// }
+
+	// if (login) {
+	//   res.status(200).json({ message: "Success", status: true, data: login });
+	// } else {
+	//   res.status(200).json({ message: "Invalid Email/Password", status: false });
+	// }
 };
 
 module.exports = { logindata };
